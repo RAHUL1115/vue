24ed41ed4c123b8e340890bf9904ad2800c13428
Rahul Gupta <52673814+RAHUL1115@users.noreply.github.com>
2020-10-08 21:29:03 +0530

    added my sql

--- main.js
+++ main.js
@@ -1,33 +1,70 @@
-Vue.component('comp1', {
-   data: function () {
-           return {
-               count: 0
-           };
-       },
-    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>',
-});
-
-const Route1 = {
-    template: '<div style = "border-radius:20px;background-color:cyan;width:200px;height:50px;margin:10px;font-size:25px;padding:10px;">This is router 1</div>'
-};
-const Route2 = {
-    template: '<div style = "border-radius:20px;background-color:green;width:200px;height:50px;margin:10px;font-size:25px;padding:10px;">This is router 2</div>'
-};
-const routes = [{
-        path: '/route1',
-        component: Route1
-    },
-    {
-        path: '/route2',
-        component: Route2
-    }
-];
-const router = new VueRouter({
-    routes: routes,
-    // mode:'history',
-});
-
 const app = new Vue({
-    el: '#app',
-    router: router,
-}); 
+    el:'#app',
+    data: {
+        paths: [],
+        time:5,
+        minurl:'',
+        url:'',
+        there : false,
+    },
+    mounted:async function(){
+        fetch('http://localhost:4000')
+        .then(response=>response.json())
+        .then((data)=>{
+            this.paths = data;
+            for (let i = 0; i < this.paths.length; i++) {
+                if (window.location.hash === '#/' + this.paths[i][0]) {
+                    this.minurl = this.paths[i][0];
+                    console.log(this.minurl);
+                    this.url = this.paths[i][1];
+                    console.log(this.url);
+                    this.there = true;
+                    this.timer();
+                    return;
+                }
+            }
+            this.minurl = window.location.hash;
+            return;
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+    },
+    methods:{
+        geturl:function(){
+            fetch('http://localhost:4000')
+                .then(response => response.json())
+                .then((data) => {
+                    this.paths = data;
+                })
+                .catch((err)=>{
+                    console.log(err);
+                });
+        },
+        addurl:function(){
+
+        },
+        timer: function () {
+            var a = setInterval(() => {
+                if(this.time <= 0){
+                    clearInterval(a);
+                    document.getElementById('ahref').click();
+                    return;
+                }
+                this.time = this.time - 1;
+            }, 1000);
+        },
+    },
+    template: `<div> 
+        <p v-if="there">if not redirected automaticly in {{time}} seconds then, click <a :href=url id='ahref' >here</a> to continue </p>
+        <div v-else-if="minurl != ''">url not found</div>
+        <div v-else>
+            <h1>enter an link to continue</h1>
+            <p><strong>enter min url</strong></input></input>: https:localhost:5500/#/<input type="text" id="minurl"></p>
+            <p><strong>enter url:</strong> </input></input><input type="text" id="url"></p>
+            <br>
+            <button @click="addurl">add</button>
+        </div>
+    </div>`
+});
+
